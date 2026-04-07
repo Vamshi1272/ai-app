be787ca728c29f5d28bb216616242bc06bf29b1e
shanmukhram <vamshivemundla@Vamshis-MacBook-Air.local>
2026-04-07 22:58:14 +0530

    fix wildcard route error

--- server/src/index.js
+++ server/src/index.js
@@ -40,7 +40,7 @@ app.use('/api/admin', adminRoutes);
 // ===== SERVE FRONTEND =====
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
 });
 
