ce30c4d99a1601f501923a156fa4e8ed1112dd6b
shanmukhram <vamshivemundla@Vamshis-MacBook-Air.local>
2026-04-07 23:01:43 +0530

    final fix wildcard route

--- server/src/index.js
+++ server/src/index.js
@@ -40,10 +40,12 @@ app.use('/api/admin', adminRoutes);
 // ===== SERVE FRONTEND =====
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 
-app.get("/*", (req, res) => {
+ // app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+// });
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
 });
-
 // PORT
 const PORT = process.env.PORT || 5001;
 
